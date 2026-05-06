67e5f8227b41a4f856030588ade51c475a4453ab
Boya Zhang <harvardboya@gmail.com>
2026-05-06 20:39:37 +0000

    add topside video logger

---
+++ video_logger/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'video_logger'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='boya',
+    maintainer_email='harvardboya@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'video_logger_node = video_logger.video_logger:main',
+        ],
+    },
+)
