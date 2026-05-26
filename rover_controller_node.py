be642c3f9f3cf3aecaf0c3c098106208d081cbfd
BoyaZhangNB <harvardboya@gmail.com>
2026-05-26 11:50:59 -0400

    implement motion conversion and cmd parsing

--- rover_controller_node.py
+++ rover_controller_node.py
@@ -1,8 +1,8 @@
 from rclpy import Node
 import rclpy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, PoseStamped
 from std_msgs.msg import String, Float32, Bool
-
+import time
 
 class RoverControllerNode(Node):
     def __init__(self):
@@ -40,7 +40,8 @@ class RoverControllerNode(Node):
 
         self.publisher = self.create_publisher(Twist, '/motion/cmd_vel', 10)
 
-        self.pose_sub = self.create_subscription(
+        self.pose_sub = self.create_subscription(PoseStamped, '/robot/pose', self.pose_callback, 10)
+        self.yaw = 0.0
 
     def cmd_vel_callback(self, msg):
         self.mbra_linear = msg.linear.x
@@ -55,12 +56,22 @@ class RoverControllerNode(Node):
 
     def turn_callback(self, msg):
         """Disable mbra and log the current orientation.
-           Calculate the target orientation.
+           Calculate the target orientation from turn angle in degrees
            Set angular velocity until pose enter desired range"""
-        
+        turn_angle = msg.data
+        target_yaw = (self.yaw + turn_angle) % 360
+        tolerance = 5.0  # degrees
+        turn_speed = 0.5  # radians per second
+        self.get_logger().info(f"Received turn command: turn_angle={turn_angle}, target_yaw={target_yaw}")
+        while self.yaw < target_yaw - tolerance or self.yaw > target_yaw + tolerance:
+            self.action_angular = turn_speed if (target_yaw - self.yaw) > 0 else -turn_speed
+            time.sleep(0.01)
+        # Polling while the rover is turning
 
     def mbra_enable_callback(self, msg):
-        pass
+        self.mbra_enabled = msg.data
+        self.get_logger().info(f"MBRA enabled: {self.mbra_enabled}")
+
 
     def publish_motion_command(self):
         cmd = Twist()
@@ -73,3 +84,7 @@ class RoverControllerNode(Node):
 
         self.publisher.publish(cmd)
         self.get_logger().info(f"Published motion command: linear_x={cmd.linear.x}, angular_z={cmd.angular.z}")
+
+    def pose_callback(self, msg):
+        self.yaw = msg.pose.orientation.z
+        self.get_logger().info(f"Received pose: yaw={self.yaw}")
