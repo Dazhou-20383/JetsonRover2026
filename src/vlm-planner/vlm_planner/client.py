c369a09204108cc94902d319555d45bbb21c1755
BoyaZhangNB <harvardboya@gmail.com>
2026-06-25 11:28:44 -0400

    set think

--- src/vlm-planner/vlm_planner/client.py
+++ src/vlm-planner/vlm_planner/client.py
@@ -34,7 +34,7 @@ class OllamaClient:
             tool_choice="auto",
             temperature=self.temperature,
             max_tokens=self.max_tokens,
-            extra_body={"keep_alive": -1}
+            extra_body={"keep_alive": -1, "think": False}
         )
 
         return response.choices[0].message
