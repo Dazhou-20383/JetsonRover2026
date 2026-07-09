cdf24ffe178467c7c8d6f68ed15e7fb0e8dc8d0e
BoyaZhangNB <harvardboya@gmail.com>
2026-07-09 19:50:49 -0400

    mod defualt arduino port

--- manuel.launch.py
+++ manuel.launch.py
@@ -3,7 +3,7 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
-
+# cli argument like ros2 launch arduino_bridge arduino_bridge.launch.py serial_port:=/dev/ttyACM0 baudrate:=115200 serial_timeout_sec:=0.1 topic_name:=/motion/motor_commands
 def generate_launch_description():
     serial_port = LaunchConfiguration('serial_port')
     baudrate = LaunchConfiguration('baudrate')
@@ -11,7 +11,7 @@ def generate_launch_description():
     topic_name = LaunchConfiguration('topic_name')
 
     return LaunchDescription([
-        DeclareLaunchArgument('serial_port', default_value='/dev/ttyACM0'),
+        DeclareLaunchArgument('serial_port', default_value='/dev/ttyAMA0'),
         DeclareLaunchArgument('baudrate', default_value='115200'),
         DeclareLaunchArgument('serial_timeout_sec', default_value='0.1'),
         DeclareLaunchArgument('topic_name', default_value='/motion/motor_commands'),
